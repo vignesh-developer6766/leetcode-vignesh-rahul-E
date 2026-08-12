62a1c3c6ee1f3de53d03c4c7c83fb80146b91513
vignesh-developer6766 <vigneshe6766@gmail.com>
2026-08-12 12:20:25 +0530

    Create 2654-CountTheNumberOfVowelStringsInRange

---
+++ 2654-CountTheNumberOfVowelStringsInRange/2654-CountTheNumberOfVowelStringsInRange.py
@@ -0,0 +1,11 @@
+# Last updated: 8/12/2026, 12:20:40 PM
+class Solution:
+    def vowelStrings(self, words, left, right):
+        vowels = "aeiou"
+        count = 0
+
+        for i in range(left, right + 1):
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                count += 1
+
+        return count
