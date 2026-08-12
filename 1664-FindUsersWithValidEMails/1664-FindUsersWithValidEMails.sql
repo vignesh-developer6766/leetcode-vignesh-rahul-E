ceecae2e513c84fc149456194d68e2a0ddae743a
vignesh-developer6766 <vigneshe6766@gmail.com>
2026-08-12 12:20:58 +0530

    Create 1664-FindUsersWithValidEMails

---
+++ 1664-FindUsersWithValidEMails/1664-FindUsersWithValidEMails.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/12/2026, 12:21:13 PM
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
