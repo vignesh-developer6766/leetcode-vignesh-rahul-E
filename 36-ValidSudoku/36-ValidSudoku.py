2f9b19cfb6a0270d3818ce5a6a72e397d8015547
vignesh-developer6766 <vigneshe6766@gmail.com>
2026-09-10 14:54:32 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -0,0 +1,30 @@
+# Last updated: 9/10/2026, 2:55:20 PM
+class Solution:
+    def isValidSudoku(self, board):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+
+                if board[r][c] == '.':
+                    continue
+
+                num = board[r][c]
+                box = (r // 3) * 3 + (c // 3)
+
+                if num in rows[r]:
+                    return False
+
+                if num in cols[c]:
+                    return False
+
+                if num in boxes[box]:
+                    return False
+
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
+
+        return True
