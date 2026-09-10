9d6a87f0151053c27a842735c2628f44aa6fc3c9
vignesh-developer6766 <vigneshe6766@gmail.com>
2026-09-10 14:54:11 +0530

    Create 83-RemoveDuplicatesFromSortedList

---
+++ 83-RemoveDuplicatesFromSortedList/83-RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,12 @@
+# Last updated: 9/10/2026, 2:54:59 PM
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
