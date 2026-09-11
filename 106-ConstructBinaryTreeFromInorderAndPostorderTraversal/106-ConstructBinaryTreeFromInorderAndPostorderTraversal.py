88214ea1e3fc94c5f0c7f6bc411cab0179395a2a
vignesh-developer6766 <vigneshe6766@gmail.com>
2026-09-11 13:44:15 +0530

    Create 106-ConstructBinaryTreeFromInorderAndPostorderTraversal

---
+++ 106-ConstructBinaryTreeFromInorderAndPostorderTraversal/106-ConstructBinaryTreeFromInorderAndPostorderTraversal.py
@@ -0,0 +1,22 @@
+# Last updated: 9/11/2026, 1:45:04 PM
+class Solution:
+    def buildTree(self, inorder, postorder):
+
+        if not inorder or not postorder:
+            return None
+
+        root = TreeNode(postorder[-1])
+
+        mid = inorder.index(postorder[-1])
+
+        root.left = self.buildTree(
+            inorder[:mid],
+            postorder[:mid]
+        )
+
+        root.right = self.buildTree(
+            inorder[mid + 1:],
+            postorder[mid:-1]
+        )
+
+        return root
